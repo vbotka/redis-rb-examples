68542dcefb94e3753c17f6649c978053a645a223
Vladimir Botka <vbotka@gmail.com>
2018-01-03 16:27:36 +0100

    RTFD html_theme switched from default to guzzle_sphinx_theme.

--- docs/source/_themes/sphinx_rtd_theme/static/js/theme.js
+++ docs/source/_themes/sphinx_rtd_theme/static/js/theme.js
@@ -16,33 +16,48 @@ function ThemeNav () {
         isRunning: false
     };
 
-    nav.enable = function () {
+    nav.enable = function (withStickyNav) {
         var self = this;
 
-        if (!self.isRunning) {
-            self.isRunning = true;
-            jQuery(function ($) {
-                self.init($);
+        if (self.isRunning) {
+            // Only allow enabling nav logic once
+            return;
+        }
 
-                self.reset();
-                self.win.on('hashchange', self.reset);
+        self.isRunning = true;
+        jQuery(function ($) {
+            self.init($);
 
+            self.reset();
+            self.win.on('hashchange', self.reset);
+
+            if (withStickyNav) {
                 // Set scroll monitor
                 self.win.on('scroll', function () {
                     if (!self.linkScroll) {
-                        self.winScroll = true;
+                        if (!self.winScroll) {
+                            self.winScroll = true;
+                            requestAnimationFrame(function() { self.onScroll(); });
+                        }
                     }
                 });
-                setInterval(function () { if (self.winScroll) self.onScroll(); }, 25);
+            }
 
-                // Set resize monitor
-                self.win.on('resize', function () {
+            // Set resize monitor
+            self.win.on('resize', function () {
+                if (!self.winResize) {
                     self.winResize = true;
-                });
-                setInterval(function () { if (self.winResize) self.onResize(); }, 25);
-                self.onResize();
+                    requestAnimationFrame(function() { self.onResize(); });
+                }
             });
-        };
+
+            self.onResize();
+        });
+
+    };
+
+    nav.enableSticky = function() {
+        this.enable(true);
     };
 
     nav.init = function ($) {
@@ -75,8 +90,15 @@ function ThemeNav () {
             })
 
         // Make tables responsive
-        $("table.docutils:not(.field-list)")
+        $("table.docutils:not(.field-list,.footnote,.citation)")
             .wrap("<div class='wy-table-responsive'></div>");
+
+        // Add extra class to responsive tables that contain
+        // footnotes or citations so that we can target them for styling
+        $("table.docutils.footnote")
+            .wrap("<div class='wy-table-responsive footnote'></div>");
+        $("table.docutils.citation")
+            .wrap("<div class='wy-table-responsive citation'></div>");
 
         // Add expand links to all parents of nested ul
         $('.wy-menu-vertical ul').not('.simple').siblings('a').each(function () {
@@ -165,7 +187,37 @@ function ThemeNav () {
 module.exports.ThemeNav = ThemeNav();
 
 if (typeof(window) != 'undefined') {
-    window.SphinxRtdTheme = { StickyNav: module.exports.ThemeNav };
+    window.SphinxRtdTheme = { Navigation: module.exports.ThemeNav };
 }
+
+
+// requestAnimationFrame polyfill by Erik Möller. fixes from Paul Irish and Tino Zijdel
+// https://gist.github.com/paulirish/1579671
+// MIT license
+
+(function() {
+    var lastTime = 0;
+    var vendors = ['ms', 'moz', 'webkit', 'o'];
+    for(var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
+        window.requestAnimationFrame = window[vendors[x]+'RequestAnimationFrame'];
+        window.cancelAnimationFrame = window[vendors[x]+'CancelAnimationFrame']
+                                   || window[vendors[x]+'CancelRequestAnimationFrame'];
+    }
+
+    if (!window.requestAnimationFrame)
+        window.requestAnimationFrame = function(callback, element) {
+            var currTime = new Date().getTime();
+            var timeToCall = Math.max(0, 16 - (currTime - lastTime));
+            var id = window.setTimeout(function() { callback(currTime + timeToCall); },
+              timeToCall);
+            lastTime = currTime + timeToCall;
+            return id;
+        };
+
+    if (!window.cancelAnimationFrame)
+        window.cancelAnimationFrame = function(id) {
+            clearTimeout(id);
+        };
+}());
 
 },{"jquery":"jquery"}]},{},["sphinx-rtd-theme"]);
--- docs/source/examples.rst
+++ docs/source/examples.rst
@@ -8,15 +8,21 @@ Create frequency graph from a log
 
 **Solution:** The loop (34) calls function *read_log* which reads the log line by line (13), splits the fields (14) and concatenate date *l[0]* and time *l[1]* in minutes (15). Third field of the log *l[2]* is status of the dpkg operation(install, upgrade, remove ...). Method *zincrby* (16) increments by 1 the score of *word* in the key *l[2]*. As a result the database contains keys(install, upgrade, remove ...) and associated lists of *words* sorted by score. Next loop (38) calls the function *write_csv* with all keys.  As a result *status.csv* files are created in the current directory with the *(date;score)* pairs.
 
-.. `[source] <../../code/create-graph-01.rb>`_
+`[create-graph-01.rb]
+<https://github.com/vbotka/redis-rb-examples/blob/master/docs/source/code/create-graph-01.rb>`_
+
+.. highlight:: Ruby
+    :linenothreshold: 5                                                                     
 
 .. literalinclude:: code/create-graph-01.rb
-   :linenos:
-   :emphasize-lines: 16, 24, 31, 38
+    :language: Ruby
+    :emphasize-lines: 16, 24, 31, 38
+    :linenos:
 
 **Result:** The *status.csv* files can be used to create a graph with *gnuplot*.
 
-.. `[source] <../../source/code/create-graph-01.gnuplot>`_
+`[create-graph-01.gnuplot]
+<https://github.com/vbotka/redis-rb-examples/blob/master/docs/source/code/create-graph-01.gnuplot>`_
 
 .. image:: code/graph-01.png
 
@@ -27,17 +33,21 @@ List 10 most used words in a text
 
 **Solution:** Let's use article about Redis at wikipedia.org as a text.
 
-.. `[source] <../../source/code/create-topchart-text.bash>`_
+`[create-topchart-text.bash]
+<https://github.com/vbotka/redis-rb-examples/blob/master/docs/source/code/create-topchart-text.bash>`_
 
 .. literalinclude:: code/create-topchart-text.bash
+    :language: Bash
 
 *zincrby* (12) increments by 1 the score of *word* in the key *topchart* and *zrange* (14) returns top 10 words with scores.
 
-.. `[source] <../../source/code/create-topchart.rb>`_
+`[create-topchart.rb]
+<https://github.com/vbotka/redis-rb-examples/blob/master/docs/source/code/create-topchart.rb>`_
 
 .. literalinclude:: code/create-topchart.rb
-   :linenos:
-   :emphasize-lines: 8, 12, 14
+    :language: Ruby
+    :emphasize-lines: 8, 12, 14
+    :linenos:
 
 **Result:**
 
@@ -54,4 +64,3 @@ List 10 most used words in a text
   34.0 to
   37.0 the
   69.0 Redis
-
